6706de7701d67e2dc209b3f381b80ee798defbaa
mamadou8bah <mbah18791@gmail.com>
2025-05-10 10:32:14 +0000

    Made changes to the service class of the publisher

--- MovieBackend/src/main/java/MovieBackend/MovieBackend/service/PublisherService.java
+++ MovieBackend/src/main/java/MovieBackend/MovieBackend/service/PublisherService.java
@@ -1,25 +1,44 @@
 package MovieBackend.MovieBackend.service;
 
-
 import MovieBackend.MovieBackend.dto.PublisherDTO;
+import MovieBackend.MovieBackend.model.Movie;
 import MovieBackend.MovieBackend.model.Publisher;
+import MovieBackend.MovieBackend.repository.MovieRepository;
 import MovieBackend.MovieBackend.repository.PublisherRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class PublisherService {
     @Autowired
     PublisherRepository publisherRepository;
 
-    public PublisherDTO getPublisherById(Long id){
-        Publisher publisher=publisherRepository.findById(id).orElseThrow(()->new RuntimeException("Publisher not found"));
+    @Autowired
+    MovieRepository movieRepository;
+
+    public PublisherDTO getPublisherById(Long id) {
+        Publisher publisher = publisherRepository.findById(id).orElseThrow(() -> new RuntimeException("Publisher not found"));
         return new PublisherDTO(publisher.getId(), publisher.getName(), publisher.getContactInfo());
     }
-    public void deletePublisher(Long id){
-        publisherRepository.deleteById(id);
+
+    @Transactional
+    public void deletePublisher(Long publisherId) {
+        Publisher publisher = publisherRepository.findById(publisherId)
+                .orElseThrow(() -> new EntityNotFoundException("Publisher not found"));
+
+
+        for (Movie movie : publisher.getMovies()) {
+            movie.setPublisher(null);
+            movieRepository.save(movie);
+        }
+
+        publisherRepository.delete(publisher);
     }
-    public Publisher addPublisher(Publisher publisher){
-       return publisherRepository.save(publisher);
+
+    public Publisher addPublisher(Publisher publisher) {
+        return publisherRepository.save(publisher);
     }
 }
